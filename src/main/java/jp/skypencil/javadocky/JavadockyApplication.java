5149dcbc5ca8cba21a3a69e0fd8f5b0483a644e1
Kengo TODA <skypencil@gmail.com>
2017-06-20 21:10:27 +0800

    initialize project with http://start.spring.io/

---
+++ src/main/java/jp/skypencil/javadocky/JavadockyApplication.java
@@ -0,0 +1,12 @@
+package jp.skypencil.javadocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadockyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadockyApplication.class, args);
+	}
+}
