1760a1a54f2227bc64aad80edb055c4ac6484f99
Kengo TODA <skypencil@gmail.com>
2017-06-25 21:44:15 +0800

    remove needless import

--- src/main/java/jp/skypencil/javadocky/LocalStorage.java
+++ src/main/java/jp/skypencil/javadocky/LocalStorage.java
@@ -14,9 +14,6 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.stereotype.Component;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
