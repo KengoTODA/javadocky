b355cb81546ea20507de607e30a3845ee28cbfaf
Kengo TODA <skypencil@gmail.com>
2017-06-28 09:09:53 +0800

    solve multi-threading problem

--- src/main/java/jp/skypencil/javadocky/JavadocDownloader.java
+++ src/main/java/jp/skypencil/javadocky/JavadocDownloader.java
@@ -3,32 +3,26 @@ package jp.skypencil.javadocky;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ReactiveHttpInputMessage;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyExtractor;
+import org.springframework.web.reactive.function.BodyExtractors;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @Component
-// FIXME ByteBuffer leak? Try -Dio.netty.leakDetection.level=advanced to get detailed report
 class JavadocDownloader {
     private static final String REPO_URL = "http://central.maven.org/maven2/";
     private WebClient webClient = WebClient.create(REPO_URL);
@@ -53,59 +47,31 @@ class JavadocDownloader {
         } else if (!status.is2xxSuccessful()) {
             return Mono.error(new IllegalArgumentException("Unexpected status code:" + status.value()));
         }
-        return res.body(new ExtractToFile()).map(Optional::of);
+        return storeToLocal(res.body(BodyExtractors.toDataBuffers())).map(Optional::of);
     }
 
-    private static class ExtractToFile implements BodyExtractor<Mono<File>, ReactiveHttpInputMessage> {
-        @Override
-        public Mono<File> extract(ReactiveHttpInputMessage inputMessage,
-                BodyExtractor.Context context) {
-            return Mono.create(emitter -> {
+    private Mono<File> storeToLocal(Flux<DataBuffer> flux) {
+        try {
+            File file = File.createTempFile("downloaded", ".jar");
+            return flux.collect(this.open(file), (FileChannel channel, DataBuffer data) -> {
                 try {
-                    File file = File.createTempFile("downloaded", ".jar");
-                    AtomicInteger size = new AtomicInteger(0);
-                    AsynchronousFileChannel channel = AsynchronousFileChannel.open(file.toPath(), StandardOpenOption.WRITE);
-                    inputMessage.getBody()
-                    .doFinally(signal -> {
-                        if (channel.isOpen()) try {
-                            channel.close();
-                        } catch (IOException e) {
-                            throw new UncheckedIOException(e);
-                        }
-                    })
-                    .map(DataBuffer::asByteBuffer)
-                    .flatMap(buffer -> {
-                        int position = size.getAndAdd(buffer.capacity());
-                        CompletableFuture<Integer> future = makeCompletableFuture(channel.write(buffer, position));
-                        return Mono.fromFuture(future);
-                    })
-                    .reduce((a, b) -> a + b)
-                    .doOnNext(total -> {
-                        log.info("Downloaded {} bytes", total);
-                    })
-                    .subscribe(total -> {
-                        if (channel.isOpen()) try {
-                            channel.close();
-                        } catch (IOException e) {
-                            emitter.error(e);
-                        }
-                        emitter.success(file);
-                    }, emitter::error);
-                } catch (RuntimeException | IOException e) {
-                    emitter.error(e);
+                    channel.write(data.asByteBuffer());
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
                 }
-            });
+            }).map(channel -> file);
+        } catch (IOException e) {
+            return Mono.error(e);
         }
+    }
 
-        // https://stackoverflow.com/a/23302308/814928
-        private <T> CompletableFuture<T> makeCompletableFuture(Future<T> future) {
-            return CompletableFuture.supplyAsync(() -> {
-                try {
-                    return future.get();
-                } catch (InterruptedException | ExecutionException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-        }
+    private Supplier<FileChannel> open(File file) {
+        return () -> {
+            try {
+                return FileChannel.open(file.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        };
     }
 }
