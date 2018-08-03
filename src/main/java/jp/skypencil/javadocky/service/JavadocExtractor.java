3c451a0ec178cd3042c7742946cc033c6f290fce
Kengo TODA <skypencil@gmail.com>
2018-08-03 17:21:27 +0800

    remove unused code

--- src/main/java/jp/skypencil/javadocky/service/JavadocExtractor.java
+++ src/main/java/jp/skypencil/javadocky/service/JavadocExtractor.java
@@ -5,31 +5,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.Enumeration;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import javax.annotation.ParametersAreNonnullByDefault;
 import jp.skypencil.javadocky.repository.Storage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuple2;
-import reactor.util.function.Tuples;
 
 /**
  * This class is responsible to download javadoc.jar and unzip its contents onto {@link Storage}.
  */
 @Service
 public class JavadocExtractor {
-  private final Logger log = LoggerFactory.getLogger(getClass());
-
   @NonNull private final JavadocDownloader downloader;
 
   @NonNull private final Storage storage;
@@ -93,92 +84,5 @@ public class JavadocExtractor {
     } catch (IOException e) {
       return Flux.error(e);
     }
-  }
-
-  Flux<Tuple2<String, Flux<ByteBuffer>>> unzip(File file) {
-    ZipFile zip;
-    try {
-      zip = new ZipFile(file);
-    } catch (IOException e) {
-      return Flux.error(e);
-    }
-    // Flux.from invokes internal lambda immediately, so use Flux.create instead
-    Flux<Tuple2<String, Flux<ByteBuffer>>> result =
-        Flux.<Tuple2<String, Flux<ByteBuffer>>>create(
-            emitter -> {
-              AtomicInteger opened = new AtomicInteger(0);
-              // When we use emitter (FluxSink), we need to consider how we should handle
-              // cancellation
-              emitter.onCancel(
-                  () -> {
-                    if (emitter.isCancelled() && opened.get() > 0) {
-                      try {
-                        zip.close();
-                      } catch (IOException e) {
-                        log.warn("Failed to close ZIP input stream", e);
-                      }
-                    }
-                  });
-
-              Enumeration<? extends ZipEntry> entries = zip.entries();
-              while (!emitter.isCancelled() && entries.hasMoreElements()) {
-                ZipEntry entry = entries.nextElement();
-                if (entry.isDirectory()) {
-                  continue;
-                }
-                try {
-                  InputStream input = zip.getInputStream(entry);
-                  Tuple2<String, Flux<ByteBuffer>> tuple = handleEntry(entry, input);
-                  opened.incrementAndGet();
-                  tuple
-                      .getT2()
-                      .doFinally(
-                          signal -> {
-                            if (opened.decrementAndGet() == 0) {
-                              // Closing ZipFile will close all InputStream created from it, so we
-                              // need to wait until all InputStreams are closed
-                              try {
-                                zip.close();
-                              } catch (IOException e) {
-                                log.warn("Failed to close ZIP input stream", e);
-                              }
-                            }
-                          });
-                  emitter.next(tuple);
-                } catch (IOException e) {
-                  emitter.error(e);
-                }
-              }
-              emitter.complete();
-            });
-    return result;
-  }
-
-  private Tuple2<String, Flux<ByteBuffer>> handleEntry(ZipEntry entry, InputStream input)
-      throws IOException {
-    String name = entry.getName();
-    // Flux.from invokes internal lambda immediately, so use Flux.create instead
-    Flux<ByteBuffer> flux =
-        Flux.create(
-            emitter -> {
-              try {
-                byte[] buffer = new byte[8 * 1024];
-                int len;
-                while (!emitter.isCancelled() && (len = input.read(buffer)) != -1) {
-                  ByteBuffer byteBuffer = ByteBuffer.wrap(Arrays.copyOf(buffer, len));
-                  emitter.next(byteBuffer);
-                }
-              } catch (IOException e) {
-                emitter.error(e);
-              } finally {
-                try {
-                  input.close();
-                } catch (IOException e) {
-                  log.warn("Failed to close InputStream from ZIP file", e);
-                }
-              }
-              emitter.complete();
-            });
-    return Tuples.of(name, flux);
   }
 }
--- src/test/java/jp/skypencil/javadocky/service/JavadocExtractorTest.java
+++ src/test/java/jp/skypencil/javadocky/service/JavadocExtractorTest.java
@@ -3,61 +3,17 @@ package jp.skypencil.javadocky.service;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 import jp.skypencil.javadocky.repository.LocalStorage;
 import jp.skypencil.javadocky.repository.Storage;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
-import reactor.util.function.Tuple2;
 
 public class JavadocExtractorTest {
   @Rule public TemporaryFolder folder = new TemporaryFolder();
-
-  @Test
-  public void testUnzip() throws IOException {
-    JavadocDownloader downloader =
-        new JavadocDownloader(folder.newFolder("javadocky-javadoc").toPath());
-    Path root = folder.newFolder("javadocky").toPath();
-    Storage storage = new LocalStorage(root);
-    JavadocExtractor extractor = new JavadocExtractor(downloader, storage);
-
-    File zip = folder.newFile();
-    try (ZipOutputStream output = new ZipOutputStream(new FileOutputStream(zip))) {
-      output.putNextEntry(new ZipEntry("hello.txt"));
-      output.write("Hello".getBytes(StandardCharsets.UTF_8));
-      output.closeEntry();
-      output.putNextEntry(new ZipEntry("world.txt"));
-      output.write("World".getBytes(StandardCharsets.UTF_8));
-      output.closeEntry();
-    }
-    Flux<Tuple2<String, Flux<ByteBuffer>>> result = extractor.unzip(zip);
-    StepVerifier.create(result)
-        .expectNextMatches(
-            tuple ->
-                tuple.getT1().equals("hello.txt")
-                    && tuple
-                        .getT2()
-                        .blockFirst()
-                        .equals(ByteBuffer.wrap("Hello".getBytes(StandardCharsets.UTF_8))))
-        .expectNextMatches(
-            tuple ->
-                tuple.getT1().equals("world.txt")
-                    && tuple
-                        .getT2()
-                        .blockFirst()
-                        .equals(ByteBuffer.wrap("World".getBytes(StandardCharsets.UTF_8))))
-        .expectComplete()
-        .verify();
-  }
 
   @Test
   public void test() throws IOException {
