2c0f9a826b6f768de5b191c7111fa32062af874f
Kengo TODA <skypencil@gmail.com>
2019-08-27 06:13:25 +0800

    test: replace vintage JUnit4 with Jupiter in integration test

--- src/integration-test/java/jp/skypencil/javadocky/PageTest.java
+++ src/integration-test/java/jp/skypencil/javadocky/PageTest.java
@@ -1,38 +1,38 @@
 package jp.skypencil.javadocky;
 
-import static com.codeborne.selenide.Selenide.element;
+import static com.codeborne.selenide.Browsers.CHROME;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
+import io.github.bonigarcia.seljup.SelenideConfiguration;
+import io.github.bonigarcia.seljup.SeleniumExtension;
 import java.util.Objects;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PageTest {
+@ExtendWith(SeleniumExtension.class)
+class PageTest {
   @LocalServerPort private int port;
-
-  @Rule public BrowserStack browserStack = new BrowserStack();
 
   /** Page page should have {@code <frameset>} to display javadoc. */
   @Test
-  public void testPageShouldHaveFrameset() {
-    open("http://localhost:" + port + "/page/jp.skypencil.guava/helper/1.0.1/");
-    assertTrue(element(By.tagName("frameset")).exists());
+  void testPageShouldHaveFrameset(
+      @SelenideConfiguration(browser = CHROME, headless = true) SelenideDriver driver) {
+    driver.open("http://localhost:" + port + "/page/jp.skypencil.guava/helper/1.0.1/");
+    assertTrue(driver.$(By.tagName("frameset")).exists());
   }
 
   /** Page page should support {@code latest} version, which redirects to specific version. */
   @Test
-  @Ignore("Not sure how to test redirect with Selenium/Selenide")
-  public void testLatestPageRedirectsToSpecificVersion() {
+  @Disabled("Not sure how to test redirect with Selenium/Selenide")
+  void testLatestPageRedirectsToSpecificVersion() {
     open("http://localhost:" + port + "/page/jp.skypencil.guava/helper/latest/");
     Selenide.Wait()
         .until(
