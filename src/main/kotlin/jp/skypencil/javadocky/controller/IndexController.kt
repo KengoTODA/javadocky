a55704fb413cdca3cb75b63ffdec89e13e41a6f6
Kengo TODA <skypencil@gmail.com>
2019-11-06 22:50:24 +0800

    refactor: replace IndexController.java with .kt

---
+++ src/main/kotlin/jp/skypencil/javadocky/controller/IndexController.kt
@@ -0,0 +1,16 @@
+package jp.skypencil.javadocky.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.core.io.ClassPathResource
+import org.springframework.stereotype.Controller
+import reactor.core.publisher.Mono
+
+@Controller
+internal class IndexController {
+    @GetMapping(path = ["/"])
+    @ResponseBody
+    fun index(): Mono<ClassPathResource> {
+        return Mono.just(ClassPathResource("static/index.html"))
+    }
+}
