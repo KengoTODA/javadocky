e6a45ce3956e2728a22970639679e9230335ecb7
Kengo TODA <skypencil@gmail.com>
2021-11-23 10:31:56 +0800

    build: introduce the Gradle build scan

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+  id("com.gradle.enterprise") version "3.7.2"
+}
+
+rootProject.name = "javadocky"
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+  }
+}
