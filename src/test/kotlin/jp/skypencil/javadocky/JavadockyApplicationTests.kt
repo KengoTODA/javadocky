c70d9c879993ed0f1f92154e2fc9e60a1647f119
Kengo TODA <skypencil@gmail.com>
2022-05-27 18:33:26 +0800

    test: migrate JavadockyApplicationTests from Java to Kotlin

---
+++ src/test/kotlin/jp/skypencil/javadocky/JavadockyApplicationTests.kt
@@ -0,0 +1,12 @@
+package jp.skypencil.javadocky
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JavadockyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
